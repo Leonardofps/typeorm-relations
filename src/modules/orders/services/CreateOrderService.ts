be824d15436ea98cea2af88a729aa4dcfe9f2118
Leonardofps <leonardo.fpsilva@hotmail.com>
2020-06-23 22:26:53 -0300

    Project finalized

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,69 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const findCustomer = await this.customersRepository.findById(customer_id);
+
+    if (!findCustomer) {
+      throw new AppError('Customer does not exists !');
+    }
+
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (!findProducts.length) {
+      throw new AppError('Product does not exist with this id');
+    }
+
+    const productExists = findProducts.map(product => product.id);
+
+    const checkProductExists = products.filter(
+      product => !productExists.includes(product.id),
+    );
+
+    if (checkProductExists.length) {
+      throw new AppError('Product not exists');
+    }
+
+    const findProductsWithoutQuantity = products.filter(
+      product =>
+        findProducts.filter(prod => prod.id)[0].quantity <= product.quantity,
+    );
+
+    if (findProductsWithoutQuantity.length) {
+      throw new AppError('The quantity of this product not available ');
+    }
+
+    const validateProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: findProducts.filter(prod => prod.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: findCustomer,
+      products: validateProducts,
+    });
+
+    const { order_products } = order;
+
+    const updatedOrderProductQuantity = order_products.map(prodord => ({
+      id: prodord.product_id,
+      quantity:
+        findProducts.filter(prod => prod.id === prodord.product_id)[0]
+          .quantity - prodord.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(updatedOrderProductQuantity);
+
+    return order;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1592940775008-CreateOrdersProducts.ts
@@ -0,0 +1,77 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProducts1592940775008
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'OrderPivot',
+            referencedTableName: 'orders',
+            referencedColumnNames: ['id'],
+            columnNames: ['order_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'ProductPivot',
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            columnNames: ['product_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrderPivot');
+    await queryRunner.dropForeignKey('orders_products', 'ProductPivot');
+
+    await queryRunner.dropColumn('orders_products', 'order_id');
+    await queryRunner.dropColumn('orders_products', 'product_id');
+
+    await queryRunner.dropTable('orders');
+  }
+}
